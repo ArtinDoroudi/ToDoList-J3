49c783a301dd15b612cb48c456e37f5c9ed120e2
isaiah Woodley <W0862252@myscc.ca>
2024-11-08 01:21:44 -0500

    Added button for submitting edited the spacing within the vbox and made the page look a little nicer

--- src/main/java/org/example/projectj3/Adding.java
+++ src/main/java/org/example/projectj3/Adding.java
@@ -25,6 +25,7 @@ public class Adding extends Application {
         Button Dashboard = new Button("Dashboard");
         Button About = new Button("About");
         Button Help = new Button("Help");
+        Button Submit = new Button("Submit");
 
         Task.setStyle("-fx-font-size: 23px; -fx-font-weight: bold;");
 
@@ -36,24 +37,39 @@ public class Adding extends Application {
 
 
 
-        Dashboard.setStyle("-fx-background-color: Translecent; -fx-border-color: Translecent; -fx-font-size: 20; -fx-font-weight: bold;");
-        About.setStyle("-fx-background-color: Translecent; -fx-border-color: Translecent; -fx-font-size: 20; -fx-font-weight: bold;");
-        Help.setStyle("-fx-background-color: Translecent; -fx-border-color: Translecent; -fx-font-size: 20; -fx-font-weight: bold;");
+        Dashboard.setStyle("-fx-background-color: Transparent; -fx-border-color: Transparent; -fx-font-size: 23; -fx-font-weight: bold;");
+        About.setStyle("-fx-background-color: Transparent; -fx-border-color: Transparent; -fx-font-size: 23; -fx-font-weight: bold;");
+        Help.setStyle("-fx-background-color: Transparent; -fx-border-color: Transparent; -fx-font-size: 23; -fx-font-weight: bold;");
+
         HBox hbox = new HBox();
         hbox.getChildren().addAll(Dashboard,About,Help);
+
         VBox vbox = new VBox();
-        vbox.getChildren().addAll(Task,TaskName,Description,TaskDescription);
+        vbox.getChildren().addAll(Task,TaskName,Description,TaskDescription, Submit);
         vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
+
+        Submit.setStyle("-fx-background-color: #8cfa8c; -fx-font-size: 15; -fx-font-weight: bold; ");
+
+
+        //VBox vbox2 = new VBox();
+        //vbox2.getChildren().add(Submit);
+        //vbox2.setAlignment(Pos.CENTER);
+        //vbox2.setSpacing(10);
+
 
         BorderPane borderPane = new BorderPane();
         borderPane.setCenter(vbox);
         borderPane.setTop(hbox);
+        //borderPane.setBottom(vbox2);
         borderPane.setStyle("-fx-background-color: TAN");
 
 
 
         Scene scene = new Scene(borderPane, 1000, 500);
-
+        //scene.getStylesheets().add(getClass().getResource("resources/css/styles.css").toExternalForm());
+        //String css = this.getClass().getResource("AddModifyStyles.css").toExternalForm();
+        //scene.getStylesheets().add(css);
         stage.setTitle("Adding New Task");
         stage.setScene(scene);
         stage.show();

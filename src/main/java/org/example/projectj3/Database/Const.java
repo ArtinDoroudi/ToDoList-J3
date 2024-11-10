eb33b5674447e064f9ff395c5d03aaa5d28b3928
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-11-10 18:44:30 -0500

    created const and database classes and added jdbc dependencies to pom.xml

---
+++ src/main/java/org/example/projectj3/Database/Const.java
@@ -0,0 +1,8 @@
+package org.example.projectj3.Database;
+
+public class Const {
+    public static final String DB_NAME = "w0111111";
+    public static final String DB_USER = "sample";
+    public static final String DB_PASSWORD = "sample";
+
+}
---
+++ src/main/java/org/example/projectj3/Database/Database.java
@@ -0,0 +1,5 @@
+package org.example.projectj3.Database;
+
+public class Database {
+
+}

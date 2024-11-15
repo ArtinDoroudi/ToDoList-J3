b11f574f36fa93689edb82e17370acb02989ecc2
parekh <w0846147@myscc.ca>
2024-11-14 23:57:03 -0800

    created a label and textfield for username

--- src/main/java/org/example/projectj3/register.java
+++ src/main/java/org/example/projectj3/register.java
@@ -17,6 +17,16 @@ public class register extends Application {
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(10);
         grid.setHgap(10);
+
+
+        // label and field for username
+        Label usernameLabel = new Label("Username:");
+        grid.add(usernameLabel, 0, 0);
+        TextField usernameField = new TextField();
+        grid.add(usernameField, 1, 0);
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 };
 

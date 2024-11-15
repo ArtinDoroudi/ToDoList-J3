5356732bf88f57214c21efb751e602e486466cba
parekh <w0846147@myscc.ca>
2024-11-15 01:53:47 -0800

    creating the read and delete databse button

--- src/main/java/org/example/projectj3/mainPage.java
+++ src/main/java/org/example/projectj3/mainPage.java
@@ -20,6 +20,19 @@ public class mainPage extends Application {
         grid.setPadding(new Insets(20));
         grid.setVgap(20);
         grid.setHgap(20);
+        grid.setAlignment(Pos.CENTER);
+        grid.setStyle("-fx-background-color: tan;"); // Set background color to tan
+
+        // Button to read from database
+        Button readButton = new Button("Read from Database");
+        readButton.setStyle("-fx-font-weight: bold; -fx-background-color: #a1d3f4; -fx-font-size: 14px; -fx-text-fill: black;");
+        grid.add(readButton, 0, 0);
+
+        // Button to delete from database
+        Button deleteButton = new Button("Delete from Database");
+        deleteButton.setStyle("-fx-font-weight: bold; -fx-background-color: #f4a1a1; -fx-font-size: 14px; -fx-text-fill: black;");
+        grid.add(deleteButton, 0, 1);
+
 
     public static void main(String[] args) {
         launch(args);

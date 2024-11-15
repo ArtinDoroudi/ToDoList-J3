cce666ed7128b24cf575dd10cd2374dc9f525224
parekh <w0846147@myscc.ca>
2024-11-15 01:42:54 -0800

    creating the main page for the project

---
+++ src/main/java/org/example/projectj3/mainPage.java
@@ -0,0 +1,18 @@
+package org.example.projectj3;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class mainPage extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

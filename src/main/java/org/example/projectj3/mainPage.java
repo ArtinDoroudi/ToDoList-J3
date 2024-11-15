8c509b4db0b10fe1f2e5e6e2a2576897a745fb33
parekh <w0846147@myscc.ca>
2024-11-15 01:46:16 -0800

    q
    creating grid oane

--- src/main/java/org/example/projectj3/mainPage.java
+++ src/main/java/org/example/projectj3/mainPage.java
@@ -11,6 +11,15 @@ import javafx.stage.Stage;
 
 public class mainPage extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Main Page");
+
+        // Create grid layout
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(20));
+        grid.setVgap(20);
+        grid.setHgap(20);
 
     public static void main(String[] args) {
         launch(args);

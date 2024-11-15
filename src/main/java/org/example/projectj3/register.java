062652c49dc0a24c1dee9344d62025586930da44
parekh <w0846147@myscc.ca>
2024-11-14 23:53:28 -0800

    created a grid pane

--- src/main/java/org/example/projectj3/register.java
+++ src/main/java/org/example/projectj3/register.java
@@ -1,4 +1,22 @@
 package org.example.projectj3;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class register {
-}
+public class register extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Register Page");
+
+        // Create grid layout
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(10);
+        grid.setHgap(10);
+    }
+};
+

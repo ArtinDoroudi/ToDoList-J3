10bcefe3bbafb26df6345029093a0cc7f9461c39
isaiah Woodley <W0862252@myscc.ca>
2024-12-01 14:58:20 -0500

    test

--- src/main/java/org/example/projectj3/GUI/mainPage.java
+++ src/main/java/org/example/projectj3/GUI/mainPage.java
@@ -2,13 +2,10 @@ package org.example.projectj3.GUI;
 
 import javafx.animation.ScaleTransition;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -17,6 +14,7 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class mainPage extends Application {
+    private VBox taskList;
 
     @Override
     public void start(Stage primaryStage) {
@@ -25,46 +23,40 @@ public class mainPage extends Application {
         Button About = new Button("About");
         Button Help = new Button("Help");
         Button createTaskButton = new Button("Create New Task");
-        CheckBox cb = new CheckBox("Complete");
-        CheckBox cb2 = new CheckBox("Complete");
-        Text text = new Text("clean your room");
-        Text text2 = new Text("finish java");
-
-        Image updateicon = new Image(getClass().getResource("/images/update4.png").toExternalForm());
-        ImageView updateview = new ImageView(updateicon);
-        Image trashcan = new Image(getClass().getResource("/images/bin.png").toExternalForm());
-        ImageView trash = new ImageView(trashcan);
-        updateview.setFitWidth(30);
-        updateview.setFitHeight(30);
-        trash.setFitWidth(30);
-        trash.setFitHeight(30);
-
-        Image updateicon2 = new Image(getClass().getResource("/images/update4.png").toExternalForm());
-        ImageView updateview2 = new ImageView(updateicon2);
-        Image trashcan2 = new Image(getClass().getResource("/images/bin.png").toExternalForm());
-        ImageView trash2 = new ImageView(trashcan2);
-        updateview2.setFitWidth(30);
-        updateview2.setFitHeight(30);
-        trash2.setFitWidth(30);
-        trash2.setFitHeight(30);
 
 
-        Button update = new Button();
-        Button delete = new Button();
-        update.setGraphic(updateview);
-        delete.setGraphic(trash);
-
-        Button update2 = new Button();
-        Button delete2 = new Button();
-        update2.setGraphic(updateview2);
-        delete2.setGraphic(trash2);
-
-        //all styles for the nodes
         Dashboard.setStyle("-fx-background-color: Transparent; -fx-border-color: Transparent; -fx-font-size: 23; -fx-font-weight: bold;");
         About.setStyle("-fx-background-color: Transparent; -fx-border-color: Transparent; -fx-font-size: 23; -fx-font-weight: bold;");
         Help.setStyle("-fx-background-color: Transparent; -fx-border-color: Transparent; -fx-font-size: 23; -fx-font-weight: bold;");
-        text.setStyle(" -fx-font-size: 20; -fx-font-weight: bold;");
-        text2.setStyle(" -fx-font-size: 20; -fx-font-weight: bold;");
+
+
+        HBox hbox = new HBox();
+        hbox.getChildren().addAll(Dashboard,About,Help);
+
+        taskList = new VBox();
+        taskList.setAlignment(Pos.CENTER);
+        taskList.setSpacing(15);
+
+
+
+        createTaskButton.setOnAction(e -> {
+            Stage addingStage = new Stage();
+            Adding addingPage = new Adding();
+            addingPage.start(addingStage);
+        });
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(taskList, createTaskButton );
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(15);
+
+
+        CheckBox cb = new CheckBox("Complete");
+        CheckBox cb2 = new CheckBox("Complete");
+
+        Text text = new Text("clean your room");
+        Text text2 = new Text("finish java");
+
 
         Dashboard.setOnMouseEntered(e -> {
             ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(200), Dashboard);
@@ -104,33 +96,6 @@ public class mainPage extends Application {
         });
 
 
-        HBox hbox = new HBox();
-        hbox.getChildren().addAll(Dashboard,About,Help);
-
-
-        HBox taskRow = new HBox();
-        taskRow.setAlignment(Pos.CENTER);
-        taskRow.getChildren().addAll(cb, text, update, delete);
-        taskRow.setStyle("-fx-background-color: LIGHTBLUE; -fx-background-radius: 10;");
-        taskRow.setSpacing(30);
-        taskRow.setMaxWidth(700);
-        taskRow.setMinHeight(50);
-
-        HBox taskRow2 = new HBox();
-        taskRow2.setAlignment(Pos.CENTER);
-        taskRow2.getChildren().addAll(cb2, text2, update2, delete2);
-        taskRow2.setStyle("-fx-background-color: LIGHTGREY; -fx-background-radius: 10;");
-        taskRow2.setSpacing(30);
-        taskRow2.setMaxWidth(700);
-        taskRow2.setMinHeight(50);
-
-
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(taskRow, taskRow2, createTaskButton );
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setSpacing(10);
-
-
         BorderPane borderPane = new BorderPane();
         borderPane.setCenter(vbox);
         borderPane.setTop(hbox);
@@ -141,6 +106,53 @@ public class mainPage extends Application {
         primaryStage.setTitle("Main Page");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private HBox addTaskRow(String Title, String description) {
+        CheckBox cb = new CheckBox("Complete");
+        Text taskTitleText = new Text(Title);
+        Text taskDescription = new Text(description);
+
+        Image updateicon2 = new Image(getClass().getResource("/images/update4.png").toExternalForm());
+        ImageView updateview2 = new ImageView(updateicon2);
+        Image trashcan2 = new Image(getClass().getResource("/images/bin.png").toExternalForm());
+        ImageView trash2 = new ImageView(trashcan2);
+        updateview2.setFitWidth(30);
+        updateview2.setFitHeight(30);
+        trash2.setFitWidth(30);
+        trash2.setFitHeight(30);
+
+        Button update = Update("/images/update4.png");
+        Button delete = Delete("/images/bin.png");
+
+        HBox taskRow = new HBox(cb, taskTitleText, update, delete
+        );
+        taskRow.setStyle("-fx-background-color: LIGHTBLUE; -fx-background-radius: 10;");
+        taskRow.setAlignment(Pos.CENTER);
+        taskRow.setSpacing(15);
+        taskRow.setMaxWidth(700);
+        taskRow.setMinHeight(50);
+
+        return taskRow;
+    }
+    private Button Delete(String path) {
+        Image trashcan2 = new Image(getClass().getResource("/images/bin.png").toExternalForm());
+        ImageView trash2 = new ImageView(trashcan2);
+        trash2.setFitWidth(30);
+        trash2.setFitHeight(30);
+        Button delete = new Button();
+        delete.setGraphic(trash2);
+        return delete;
+    }
+
+    private Button Update(String path) {
+        Image updateicon2 = new Image(getClass().getResource("/images/update4.png").toExternalForm());
+        ImageView updateview2 = new ImageView(updateicon2);
+        updateview2.setFitWidth(30);
+        updateview2.setFitHeight(30);
+        Button update = new Button();
+        update.setGraphic(updateview2);
+        return update;
     }
 
     public static void main(String[] args) {
